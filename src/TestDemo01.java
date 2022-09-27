08cb40269b3ad7282ce17c591c12573a57320c31
priyasri01 <Priyasri.april14@gmail.com>
2022-09-27 18:37:17 -0400

    first commit

---
+++ src/TestDemo01.java
@@ -0,0 +1,15 @@
+
+public class TestDemo01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//write your code here
+		
+		for(int i=0; i<=10; i++) {
+			
+			System.out.println(i);
+		}
+	}
+
+}
